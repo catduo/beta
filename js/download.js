8a0a880df1f602ebfc0385188494ca79010496ea
David Geisert <dg@catduo.com>
2014-07-16 13:52:59 -0700

    added all fish cook platforms

--- js/download.js
+++ js/download.js
@@ -5,40 +5,64 @@
     {
        if (navigator.appVersion.indexOf("Win")!=-1) {
           document.getElementById(toDownload).innerHTML = "Download Windows Game";
-          url = "/PCgame.zip";
+          url = "nPC.zip";
        }
-       if (navigator.appVersion.indexOf("Mac")!=-1) {
+       else if (navigator.appVersion.indexOf("Mac")!=-1) {
           document.getElementById(toDownload).innerHTML = "Download MacOS Game";
-          url = "/game.app";
+          url = "nMac.zip";
        }
-       if (navigator.appVersion.indexOf("X11")!=-1) {
+       else if (navigator.appVersion.indexOf("X11")!=-1) {
           document.getElementById(toDownload).innerHTML = "Nothing for Unix";
           url = "";
        }
-       if (navigator.appVersion.indexOf("Linux")!=-1) {
+       else if (navigator.appVersion.indexOf("Linux")!=-1) {
           document.getElementById(toDownload).innerHTML = "Download Linux Game";
-          url = "/Linuxgame.zip";
+          url = "nLinux.zip";
        }
-       if (navigator.appVersion.indexOf("iPhone")!=-1 || navigator.appVersion.indexOf("iPod")!=-1 || navigator.appVersion.indexOf("iPad")!=-1) {
-          document.getElementById(toDownload).innerHTML = "Download iPhone Controller";
-          url = "Controller.ipa";
+       else if (navigator.appVersion.indexOf("iPhone")!=-1 || navigator.appVersion.indexOf("iPod")!=-1 || navigator.appVersion.indexOf("iPad")!=-1) {
+          document.getElementById(toDownload).innerHTML = "Download iPhone Game";
+          url = "n.ipa";
        }
-       if (navigator.appVersion.indexOf("Android")!=-1) {
-          document.getElementById(toDownload).innerHTML = "Download Android Controller";
-          url = "Controller.apk";
+       else if (navigator.appVersion.indexOf("Android")!=-1) {
+          document.getElementById(toDownload).innerHTML = "Download Android Game";
+          url = "n.apk";
        }
-       if (navigator.appVersion.indexOf("BlackBerry")!=-1) {
+       else if (navigator.appVersion.indexOf("BlackBerry")!=-1) {
           document.getElementById(toDownload).innerHTML = "Nothing for BlackBerry";
           url = "";
        }
     }
-    function Download(toDownload){ 
-       var iframe = document.createElement("iframe"); 
-       if(url.length > 0){
-    	   if(url.charAt(0) == "/"){
-    	       iframe.src = toDownload + url;
-    	       iframe.style.display = "none"; 
-    	       document.body.appendChild(iframe);
-    	   }
-       }
+    var controllerUrl = "";
+    function GetDownloadController(){
+        if (navigator.appVersion.indexOf("iPhone")!=-1 || navigator.appVersion.indexOf("iPod")!=-1 || navigator.appVersion.indexOf("iPad")!=-1) {
+            document.getElementById("downloadController").innerHTML = "Download iPhone Controller";
+            controllerUrl = "../../Controller.ipa";
+         }
+        else if (navigator.appVersion.indexOf("Android")!=-1) {
+            document.getElementById("downloadController").innerHTML = "Download Android Controller";
+            controllerUrl = "../../Controller.apk";
+         }
+        else{
+        	var element = document.getElementById("downloadController");
+        	element.parentNode.removeChild(element);
+        }
     }
+    function Download(toDownload){ 
+        var iframe = document.createElement("iframe"); 
+        if(url.length > 0){
+     	   if(url[0] == 'n'){
+     		   url = toDownload + url.substring(1);
+     	   }
+ 	       iframe.src = url;
+ 	       iframe.style.display = "none"; 
+ 	       document.body.appendChild(iframe);
+        }
+     }
+    function DownloadController(){ 
+        var iframe = document.createElement("iframe"); 
+        if(url.length > 0){
+ 	       iframe.src = controllerUrl;
+ 	       iframe.style.display = "none"; 
+ 	       document.body.appendChild(iframe);
+        }
+     }
